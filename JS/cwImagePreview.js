b1c1cbf586b6ffd46d1c33d4f9afeafd750b1395
cww978 <492886274@qq.com>
2017-09-12 21:41:06 +0800

    2013

---
+++ JS/cwImagePreview.js
@@ -0,0 +1,11 @@
+var ImagePreview = function(){
+    var my_canvas = document.getElementsByClassName('photo-canvas')[0];
+    var my_ctx = my_canvas.getContext('2d');
+    function init(){
+        
+    };
+    function changeImage(){};
+    function moveImage(){};
+    function showNext(){};
+    function showUp(){}
+};
